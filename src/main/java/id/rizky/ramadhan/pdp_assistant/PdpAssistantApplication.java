b9cab4941bd2316a34172ddd1676052ee6319387
Rizky Ramadhan <rizkyyud27@gmail.com>
2026-08-20 10:29:12 +0700

    chore: init project + ollama setup

---
+++ src/main/java/id/rizky/ramadhan/pdp_assistant/PdpAssistantApplication.java
@@ -0,0 +1,13 @@
+package id.rizky.ramadhan.pdp_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/rizky/ramadhan/pdp_assistant/PdpAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package id.rizky.ramadhan.pdp_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
